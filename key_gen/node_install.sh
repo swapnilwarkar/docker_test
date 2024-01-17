92a8bf6d5621a2ff173b9f56e5d59cf6dc810d82
warkargs <swapnil.warkar@gslab.com>
2024-01-18 00:03:20 +0530

    checkin the features - documents plus ffmpeg container

---
+++ key_gen/node_install.sh
@@ -0,0 +1,13 @@
+node upgrade
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+vi ~/.bashrc
+nvm list
+nvm install 
+nvm install 21.1.0
+npm install
+npm --version
+node --version
+npm run build
+npm start
+code application.code-workspace
