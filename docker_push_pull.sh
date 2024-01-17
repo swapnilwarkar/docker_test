6b6f1cc8dbd81640666d771a956dae7bc533b112
warkargs <swapnil.warkar@gslab.com>
2024-01-17 23:50:15 +0530

    checkin the features - documents plus ffmpeg container

---
+++ docker_push_pull.sh
@@ -0,0 +1,9 @@
+sudo docker login ghcr.io -u swapnilwarkar -p TOKEN
+
+sudo docker build -t ghcr.io/swapnilwarkar/my-ffmpeg-image:v1.0 .
+
+sudo docker push ghcr.io/swapnilwarkar/my-ffmpeg-image:v1.0
+
+# https://github.com/users/swapnilwarkar/packages/container/package/my-ffmpeg-image
+
+sudo docker pull ghcr.io/swapnilwarkar/my-ffmpeg-image:v1.0
