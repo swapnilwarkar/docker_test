fbfc73d27924b6e8df19f52726365c0c8bdd91dc
warkargs <swapnil.warkar@gslab.com>
2024-01-18 11:04:55 +0530

    checkin the features - documents plus ffmpeg container

--- key_gen/node_install.sh
+++ key_gen/node_install.sh
@@ -1,3 +1,4 @@
+
 node upgrade
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -13,4 +14,6 @@ npm start
 npm test
 code application.code-workspace
 
+# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 #rm -rf node_modules/
