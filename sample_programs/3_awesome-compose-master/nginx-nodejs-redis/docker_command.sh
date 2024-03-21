1e92df4019b9ae4e8ab19eb2dc9065fd2200e6e6
swapnilwarkar@gmail.com <>
2024-03-21 17:32:02 +0530

    checkin the features - multiple docker files are added

---
+++ sample_programs/3_awesome-compose-master/nginx-nodejs-redis/docker_command.sh
@@ -0,0 +1,11 @@
+docker compose up -d
+docker compose ps
+#curl localhost:8000
+
+docker info
+docker images
+docker ps
+docker ps -a
+
+#docker compose down
+
---
+++ sample_programs/asterisk/docker_command.sh
@@ -0,0 +1,13 @@
+docker-compose build
+docker-compose up -d
+
+#docker compose up -d
+docker compose ps
+
+docker info
+docker images
+docker ps
+docker ps -a
+
+#docker compose down
+
