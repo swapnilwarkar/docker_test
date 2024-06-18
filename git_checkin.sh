ac05979bab1b0ee6802d51b023c852799df6bd1b
swapnilwarkar@gmail.com <>
2024-06-18 09:46:42 +0530

    checkin the features - my docker course next version 8

--- git_checkin.sh
+++ git_checkin.sh
@@ -1,7 +1,7 @@
 git branch
 git status 
 git add . 
-git commit -m "checkin the features - my docker course is attached ver7" 
+git commit -m "checkin the features - my docker course next version 8" 
 git push origin main 
 git status
 git log -n2
